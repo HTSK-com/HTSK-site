80084f9ad5e45cab89abc6889f4d723d9817dd64
yeezy_na_izi <howone1sky@gmail.com>
2021-05-02 10:41:06 +0300

    Big commit add first test in tests.py add first url in urls.py add def in views.py which rendered html remove templates directory and add simple home.html

--- HTSK/portfolio/tests.py
+++ HTSK/portfolio/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+    """Тест домашней страницы"""
+
+    def test_uses_home_template(self):
+        """тест: используется домашний шаблон"""
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
