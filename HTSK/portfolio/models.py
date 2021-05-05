177c9d713be29aa69d04bb1f46f9becdd5df1600
Syrochek <syrov.art@gmail.com>
2021-05-05 19:10:47 +0300

    add some tests modified models.py: solve problem with image defaults

--- HTSK/portfolio/models.py
+++ HTSK/portfolio/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.files import File
 
 
 # Create your models here.
@@ -6,7 +7,8 @@ class Works(models.Model):
     class Meta:
         verbose_name = 'Работа'
         verbose_name_plural = 'Работы'
-    name_project = models.CharField('Название проекта', default='', max_length=100)
-    git_link = models.URLField('Ссылка на GitRep', default='')
-    photo_path = models.ImageField('картинка работа', upload_to='portfolio/static/work')
-    description = models.TextField('Описание', default='')
+    name_project = models.CharField('Название проекта', default='work', max_length=100)
+    git_link = models.URLField('Ссылка на GitRep', default='https://github.com')
+    photo_path = models.ImageField('картинка работа', default=File(open('portfolio/static/logo.jpg', mode='rb')),
+                                   upload_to='portfolio/static/work')
+    description = models.TextField('Описание', default='something...')
--- HTSK/portfolio/tests.py
+++ HTSK/portfolio/tests.py
@@ -1,10 +1,28 @@
 from django.test import TestCase
+from .models import Works
 
 
-class HomePageTest(TestCase):
+class TemplateUseTest(TestCase):
     """Тест домашней страницы"""
 
     def test_uses_home_template(self):
         """тест: используется домашний шаблон"""
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'index.html')
+
+    # def test_uses_our_works_template(self):
+    #     """тест: используется рабочий шаблон"""
+    #     response = self.client.get('our_works')
+    #     self.assertTemplateUsed(response, 'our_works.html')
+
+
+class WorkModelTest(TestCase):
+    """Тест модели work"""
+
+    def test_saving_and_retrieving_items(self):
+        """тест: сохранение и получение элементов"""
+        Works.objects.create()
+        save_items = Works.objects.first()
+        self.assertEqual(save_items.name_project, 'work')
+
+
